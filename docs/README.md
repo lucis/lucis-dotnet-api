d6abb2ceb12abf00151de48f83c94934120f17d1
Luciano Júnior <luciannojunior@gmail.com>
2020-02-17 23:17:31 -0300

    My first DotNet app

---
+++ docs/README.md
@@ -0,0 +1,5 @@
+# Lucis Dotnet Api
+
+Hey you all! This is a simple DotNet app on VTEX IO.
+
+~DotNet seems so much prettier than .NET~
---
+++ dotnet/Controllers/RoutesController.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace service.Controllers
+{
+    public class RoutesController : Controller
+    {
+        public string LucisHandler()
+        {
+            return "{\"Name\": \"Lucis\", \"CanImportADotNetJSONDependency\": false}";
+        }
+
+    }
+}
